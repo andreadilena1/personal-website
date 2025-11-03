5788d889554c82ae82d776ed040e004c4cc6b9dd
andreadilena1 <a.dilena@elitesoftwarehouse.com>
2025-11-03 09:36:22 +0100

    Updates

--- frontend/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Footer() {
-  return (
-    <footer className="bg-gray-100 text-center text-sm text-gray-500 py-4">
-      © {new Date().getFullYear()} Andrea Di Lena. Tutti i diritti riservati.
-    </footer>
-  );
-}
