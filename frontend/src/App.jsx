c774af64da44a5d0a70429b81c1ae710056371f0
andreadilena1 <a.dilena@elitesoftwarehouse.com>
2025-11-03 10:15:18 +0100

    Usa HashRouter e configura Vite per GitHub Pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HomePage from './components/HomePage';
 import About from './components/About';
