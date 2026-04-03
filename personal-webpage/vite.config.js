1723b3af3637530a0d5823adbd51fef536db1c62
andreadilena1 <a.dilena@elitesoftwarehouse.com>
2026-04-03 17:20:36 +0200

    update correct build for GH Pages

--- personal-webpage/vite.config.js
+++ personal-webpage/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
+  base: '/personal-website/', // <-- obbligatorio per GitHub Pages
   plugins: [react()],
-  base: './',
-}))
+});
